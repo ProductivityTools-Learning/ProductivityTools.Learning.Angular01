06f959e07ecf9a7a9135e3a0ff74943af9c0fdc5
Pawel Wujczyk <pwujczyk@gmail.com>
2025-05-29 23:13:43 +0200

    service and tests

--- angular-hello-world/src/app/home-page/home-page.component.ts
+++ angular-hello-world/src/app/home-page/home-page.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { ProductService } from '../product.service';
+import { NgFor } from '@angular/common';
+import { Product } from '../models/Product';
 
 @Component({
   selector: 'app-home-page',
-  imports: [],
+  imports: [NgFor],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css'
 })
 export class HomePageComponent {
+  products: Product[]
 
+  constructor(productService: ProductService) {
+    this.products = productService.getProducts();
+  }
 }
---
+++ angular-hello-world/src/app/product.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { Product } from './models/Product';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  products: Product[] = [
+    new Product('Apple', 10),
+    new Product('Orrange', 20),
+    new Product('Bannana', 30)
+  ]
+
+  getProducts = () => {
+    return this.products;
+  }
+  constructor() { }
+}
